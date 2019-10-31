64a1a862b6c6b76b6e89a25b130e9a6fa0d5f2c4
Jaeriah <jaeriahpasseri@gmail.com>
2019-10-31 11:31:58 -0700

    added bootstrap card image class

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,10 +2,10 @@
 <template>
     <div class="card my-5">
         <div class="card-body">
-            <img :src="product.media.source" :alt="product.name" class="text-center"/>
+            <img :src="product.media.source" :alt="product.name" class="card-img-top"/>
             <h4 class="title text-center card-title display-5">{{product.name}}</h4>
-            <p class="description text-center card-title display-5" v-html="product.description"></p>
-            <p class="text-center text-muted card-text display-5">{{product.price.formatted_with_symbol}}</p>
+            <p class="description text-center card-text display-5" v-html="product.description"></p>
+            <p class="text-center text-muted card-subtitle display-5">{{product.price.formatted_with_symbol}}</p>
             <button :disabled="isInWishlist" @click="$emit('add-to-wishlist',product)" class="btn btn-info form-control"><i class="fas fa-heart"></i>{{isInWishlist ? 'Added to wishlist': 'Add to Wishlist'}}
             </button>
         </div>
