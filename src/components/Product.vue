cd87ee9b6ccd766e31492ebe896bbde07d38175b
Jaeriah <jaeriahpasseri@gmail.com>
2019-10-31 01:22:33 -0700

    parsed out product media image

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="card my-5">
         <div class="card-body">
-            <img :src="product.media.image" :alt="product.name" />
+            <img :src="product.media.source" :alt="product.name" />
             <h4 class="title text-center card-title display-5">{{product.name}}</h4>
             <p class="description text-center card-title display-5">{{product.description}}</p>
             <p class="text-center text-muted card-text display-5">{{product.price.formatted_with_symbol}}</p>
