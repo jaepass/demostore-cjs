d5a57d26e405fcbf61ee0ac9b90856c4ab7f9e7a
Jaeriah <jaeriahpasseri@gmail.com>
2019-10-30 23:59:03 -0700

    fixed wishlist attributes

--- src/components/Wishlist.vue
+++ src/components/Wishlist.vue
@@ -7,7 +7,7 @@ to send data back to parent component
   <div>
     <ul class="list-group">
       <li class="list-group-item" v-for="item in items" :key="item.id">
-        {{ item.title }} - $ {{ item.price }}
+        {{ item.name }} {{ item.price.formatted_with_symbol }}
         <button
           @click="$emit('remove-from-wishlist',item)"
           class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded float-right"
